e5f6b90be8a4e6e08b12990ff12e4e8866538103
jonasjuerss <jonas.juerss@tum.de>
2023-01-12 17:32:34 +0000

    Moved dataloader initialization

--- dataloader.py
+++ dataloader.py
@@ -72,36 +72,3 @@ class FrameGenerator:
             yield frames_from_video_file(os.path.join(self.path_groundtruth, folder),
                                          os.path.join(self.path_generated, folder),
                                          self.n_frames)
-
-output_signature = (tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32),
-                    tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32),
-                    tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32))
-
-train_ds = tf.data.Dataset.from_generator(FrameGenerator("HR", "tecogan_generate", training=True),
-                                          output_signature=output_signature)
-val_ds = tf.data.Dataset.from_generator(FrameGenerator("HR", "tecogan_generate", training=True),
-                                        output_signature=output_signature)
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-
-for a in train_ds.take(10):
-    print(*a)
-
-train_frames, train_labels = next(iter(train_ds))
-print(f'Shape of training set of frames: {train_frames.shape}')
-print(f'Shape of training labels: {train_labels.shape}')
-
-val_frames, val_labels = next(iter(val_ds))
-print(f'Shape of validation set of frames: {val_frames.shape}')
-print(f'Shape of validation labels: {val_labels.shape}')
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True),
-              metrics=['accuracy'])
-
-model.fit(train_ds,
-          epochs = 10,
-          validation_data = val_ds,
-          callbacks = tf.keras.callbacks.EarlyStopping(patience = 2, monitor = 'val_loss'))
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 import wandb
 
+from dataloader import FrameGenerator
 from wandb_utils import init_wandb
 
 parser = argparse.ArgumentParser(description='DNN curve training')
@@ -18,18 +19,35 @@ if __name__ == "__main__":
     args = parser.parse_args()
     init_wandb(args)
 
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=args.seed,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+    output_signature = (tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32),
+                        tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32),
+                        tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32))
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=args.seed,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+    train_ds = tf.data.Dataset.from_generator(FrameGenerator("HR", "tecogan_generate", training=True),
+                                              output_signature=output_signature)
+    val_ds = tf.data.Dataset.from_generator(FrameGenerator("HR", "tecogan_generate", training=True),
+                                            output_signature=output_signature)
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+
+    for a in train_ds.take(10):
+        print(*a)
+
+    train_frames, train_labels = next(iter(train_ds))
+    print(f'Shape of training set of frames: {train_frames.shape}')
+    print(f'Shape of training labels: {train_labels.shape}')
+
+    val_frames, val_labels = next(iter(val_ds))
+    print(f'Shape of validation set of frames: {val_frames.shape}')
+    print(f'Shape of validation labels: {val_labels.shape}')
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    model.fit(train_ds,
+              epochs=10,
+              validation_data=val_ds,
+              callbacks=tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'))
